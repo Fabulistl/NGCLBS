f9d0ec1134bfaab197bab0f00f476ba3d6c46f47
Fabulistl <renjianshigewhl@gmail.com>
2025-06-18 11:18:43 +0800

    1

---
+++ src/page_data/Link.ts
@@ -0,0 +1,19 @@
+export default {
+  // API 接口请求优先，数据格式保持和 data 一致
+  api: 'kkk',
+  // api 为空则使用 data 静态数据
+  data: [
+    {
+      "name": "NGC设计院",
+      "link": "https://www.ngclabs.cn/",
+      "avatar": "https://mmbiz.qpic.cn/sz_mmbiz_png/TNFTMPLtgGlwld5jmyuibmicw1Opnc6oVhLKXqYRyuy60icspjga5ZAPeJiasXWXiajicBKE2mwfUKo2BstSP8oC3SPA/300?wx_fmt=png&wxfrom=18",
+      "descr": "运气是计划之外的东西."
+    },
+    {
+      "name": "星云网络计算",
+      "link": "https://www.ngclabs.cn/",
+      "avatar": "https://mmbiz.qpic.cn/sz_mmbiz_png/hXOvVUCllwjTBPVDSsUnrjDzuG3f99fpNAgIKYibiaM2xO0zB4mFogT13l9FyYjkXseiaA4G3pNmaoPgNlNjj9jXw/300?wx_fmt=png&wxfrom=18",
+      "descr": "免费Web API数据接口调用服务平台."
+    }
+  ]
+}
